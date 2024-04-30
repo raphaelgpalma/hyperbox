9b1c65e55dd388b1366ebf5e2c88437fd7ef3f0b
raphaelgpalma <raphaelgpalma@icloud.com>
2024-04-30 14:36:06 -0300

    Made homepage interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import logoImg from './logo.png';
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
+      <img src={logoImg} alt="Logo" />;
       <SearchBar placeholder="Seach with HyperCube" />
     </div>
   );
